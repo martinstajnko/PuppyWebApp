4875c54ce81e07a324ae0b1e8702c77096f32706
Martin <stajnko.martin93@gmail.com>
2021-01-27 20:55:45 +0100

    Added addowner.html file and modified app.py, form.py and rest of .html files

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from forms import  AddForm , DelForm
+from forms import  AddForm , DelForm, AddOwnerForm
 from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -22,14 +22,40 @@ Migrate(app,db)
 class Puppy(db.Model):
 
     __tablename__ = 'puppies'
-    id = db.Column(db.Integer,primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
+
+    # This is a one-to-one relationship
+    # A puppy only has one owner, thus uselist is False.
+    # Strong assumption of 1 dog per 1 owner and vice versa.
+    owner = db.relationship('Owner',backref='puppy',uselist=False)
 
     def __init__(self,name):
         self.name = name
 
     def __repr__(self):
-        return f"Puppy name: {self.name}" #w with owner: {self.owner}
+        if self.owner:
+            return f"Puppy name: {self.name} with owner {self.owner.name}"
+        else:
+            return f"Puppy name: {self.name} has no owner."
+
+
+class Owner(db.Model):
+
+    __tablename__ = 'owners'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+
+    # One-to-one relationship with class Puppy
+    puppy_id = db.Column(db.Integer, db.ForeignKey('puppies.id'))
+
+    def __init__(self, name, puppy_id): 
+        self.name = name
+        self.puppy_id = puppy_id
+
+        def __repr__(self):
+            return f"owner name: {self.owner}"
+
 
 ############################################
 
@@ -53,7 +79,6 @@ def add_pup():
         db.session.commit()
 
         return redirect(url_for('list_pup'))
-
     return render_template('add.html',form=form)
 
 @app.route('/list')
@@ -75,6 +100,20 @@ def del_pup():
 
         return redirect(url_for('list_pup'))
     return render_template('delete.html',form=form)
+
+@app.route('/addowner', methods=['GET', 'POST'])
+def add_owner():
+
+    form = AddOwnerForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        pup_id = form.pup_id.data
+        new_owner = Owner(name, pup_id)
+        db.session.add(new_owner)
+        db.session.commit()
+        return redirect(url_for('list_pup'))
+    return render_template('addowner.html', form=form)
 
 
 if __name__ == '__main__':
